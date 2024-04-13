730ae377251eae532ec81bb738746827ad5af1bc
IanTayler <iangtayler@gmail.com>
2024-04-13 00:58:17 -0300

    Toggle off terminal if currently active

--- lua/terminal_fns.lua
+++ lua/terminal_fns.lua
@@ -1,5 +1,10 @@
 local term_prefix = "term://"
 
+function is_term_bufname(name)
+  return name:sub(1, #term_prefix) == term_prefix
+end
+
+
 function in_bottom_split(fn)
   vim.cmd("split")
   vim.cmd.wincmd("j")
@@ -12,10 +17,17 @@ function new_terminal()
 end
 
 function toggle_terminal()
+  local current_buf = vim.api.nvim_buf_get_name(0)
+
+  if is_term_bufname(current_buf) then
+    vim.cmd.wincmd("c")
+    return
+  end
+
   for i, buf_hndl in ipairs(vim.api.nvim_list_bufs()) do
     local name = vim.api.nvim_buf_get_name(buf_hndl)
 
-    if name:sub(1, #term_prefix) == term_prefix then
+    if is_term_bufname(name) then
       local window = vim.fn.bufwinid(buf_hndl)
       if window > 0 then
         vim.api.nvim_set_current_win(window)
